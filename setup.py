69dd83c249f18efe96f0dcd8058efcd1f24d5066
Jack Pee <mcinvasion2016@gmail.com>
2022-01-04 21:09:48 -0700

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="cheinstein",
+    version="0.1.0",
+    author="jckli",
+    description="A Python library to get information from Chegg.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DouglasTaylorSupportGroup/cheinstein.py",
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "beautifulsoup4"]
+)
