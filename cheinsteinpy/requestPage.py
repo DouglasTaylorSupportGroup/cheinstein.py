d8ca024508335bdc9eeeb0c7af56fc1adefadbae
Jack Li <jack@jackli.dev>
2022-02-16 23:34:07 -0700

    merge

--- cheinsteinpy/requestPage.py
+++ cheinsteinpy/requestPage.py
@@ -58,3 +58,23 @@ def requestChapter(url, cookie, userAgent, html):
     url = 'https://www.chegg.com/study/_ajax/persistquerygraphql'
     response = requests.post(url=url, headers=headers, json=query, data=None)
     return response.json()
+
+def requestEnhancedAnswer(url, cookie, userAgent, html):
+    headers = {
+        'Host': 'www.chegg.com',
+        'authority': 'www.chegg.com',
+        "Accept-Encoding": "gzip, deflate, br",
+        'accept-language': 'en-US,en;q=0.5',
+        'cookie': cookie,
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+        'sec-ch-ua-mobile': '?0',
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'user-agent': userAgent,
+        'referer': 'https://www.google.com/'
+    }
+    
